fc3a3f8d82c3418371442cd96311c8160a9c5ef9
pkbmgnus <140223952+pkbmgnus@users.noreply.github.com>
2023-10-14 07:47:49 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,21 +2,21 @@
 const nextConfig = {};
 // const withFonts = require('next-fonts');
 module.exports = {
-   output: 'out',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'pkbmgnus.sch.id',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pkbmgnus.sch.id',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    output: 'export',
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'http',
+  //       hostname: 'pkbmgnus.sch.id',
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'pkbmgnus.sch.id',
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //   ],
+  // },
 };
